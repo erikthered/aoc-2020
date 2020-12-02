7d7a52e17ab2f7a2fe9b309a146e45c88cb0742f
Erik Nelson <erik.david.nelson@gmail.com>
2020-12-01 21:02:25 -0500

    Scaffold project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.20"
+}
+
+group = "com.github.erikthered"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
