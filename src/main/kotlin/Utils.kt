eabd355dcfbd7ace0c48624080ca8c5c3624f867
Erik Nelson <erik.david.nelson@gmail.com>
2020-12-08 00:50:55 -0500

    Add additional input parser function

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,5 @@
 import java.io.File
 
 fun getInputLines(fileName: String) = File(ClassLoader.getSystemResource(fileName).toURI()).readLines()
+
+fun getInputString(fileName: String) = File(ClassLoader.getSystemResource(fileName).toURI()).readText()
