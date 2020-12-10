b15720e5563520713c56c11e531036d6e6434c6f
Erik Nelson <erik.david.nelson@gmail.com>
2020-12-10 16:45:57 -0500

    Day 9 part 2

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -24,4 +24,19 @@ fun main() {
     } while (!exit)
 
     println("Part 1: ${input[cursor]}")
+
+    val sum = input[cursor]
+
+    loop@ for (i1 in input.indices) {
+        for (i2 in (i1+1)..input.size) {
+            val slice = input.slice(i1..i2)
+            if (slice.sum() == sum) {
+                println("Part 2: Min: ${slice.minOrNull()}, Max: ${slice.maxOrNull()}, Sum: ${(slice.minOrNull() ?: 0) + (slice.maxOrNull() ?: 0)}")
+                break@loop
+            }
+            if (slice.sum() > sum) {
+                break
+            }
+        }
+    }
 }
