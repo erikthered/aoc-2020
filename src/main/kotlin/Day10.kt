52864ab6cd4a9d9f30f5cc1d12a3b57b53794615
Erik Nelson <erik.david.nelson@gmail.com>
2020-12-12 20:21:14 -0500

    Day 10 part 2

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,22 +1,28 @@
 fun main() {
     val input = getInputLines("day10.txt").map(String::toInt)
 
-    val candidates = input.sorted()
-
-    var joltage = 0
-    val target = candidates.maxOrNull()!! + 3
-
+    val joltages = listOf(0) + input.sorted() + listOf(input.maxOrNull()!! + 3)
     val differences = mutableMapOf<Int, Int>()
 
-    differences[candidates.minOrNull()!!] = 1
-    differences[3] = 1
-
-    candidates
+    joltages
         .windowed(2, 1)
         .forEach {
             val diff = it[1] - it[0]
             differences[diff] = differences.computeIfAbsent(diff) { 0 } + 1
         }
 
-    println("Part 1: ${differences.get(1)} * ${differences.get(3)} = ${differences[1]!! * differences[3]!!}")
+    println("Part 1: ${differences[1]} * ${differences[3]} = ${differences[1]!! * differences[3]!!}")
+
+    val combinations = mutableMapOf<Int, Long>()
+    joltages.forEach { joltage ->
+        combinations[joltage] = if (joltage == 0) {
+            1
+        } else {
+            joltages
+                .filter { it < joltage && it >= joltage - 3 }
+                .map { combinations[it] ?: 1 }
+                .sum()
+        }
+    }
+    println(combinations[joltages.last()])
 }
